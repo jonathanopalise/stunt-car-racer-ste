bb779d8413f7a6c73c3c7c8e2469e87f69906d53
Jonathan Thomas <jonathan@opalise.co.uk>
2021-09-03 16:42:41 +0100

    simplify/optimise

--- 0x80000_sky.s
+++ 0x80000_sky.s
@@ -1,7 +1,7 @@
+
     ORG $80000
 
     move.l a0,-(a7)
-    move.l d0,-(a7)
 
     addq.l #1,d4
     lea $ffff8a28.w,a4
@@ -15,24 +15,19 @@
     addq.l #2,a4
     move.w #$f,(a4)+    ; 8a3a.w hop/op
 
-    ; at this point, a4 contains address of control
-    ; a0 contains destination
-
     move.l $518a4,a0
-    moveq.l #3,d0
 
     rept 3
     move.l a0,-10(a4)   ; 8a32.l destaddress
     move.w d4,-4(a4)    ; 8a38.w ycount
     move.b #$c0,(a4)    ; 8a3c.b control
-    addq.l #2,a0
+    addq.l #2,a0        ; move to next plane
     endr
     move.l a0,-10(a4)   ; 8a32.l destaddress
     move.w d4,-4(a4)    ; 8a38.w ycount
-    move.w #$0,-2(a4)     ; 8a3a.w hop/op
+    clr.w -2(a4)        ; 8a3a.w hop/op
     move.b #$c0,(a4)    ; 8a3c.b control
 
-    move.l (a7)+,d0     ; restore d0
     move.l (a7)+,a0     ; restore a0
 
     jmp $4f2f6

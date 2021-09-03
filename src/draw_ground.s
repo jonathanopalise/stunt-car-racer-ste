3e03be62aee84668a58c63ced51490fceeae8df2
Jonathan Thomas <jonathan@opalise.co.uk>
2021-09-03 19:28:24 +0100

    4 passes on ground (wrong colour)

--- src/draw_ground.s
+++ src/draw_ground.s
@@ -1,16 +1,32 @@
 draw_ground:
+
+    move.l a0,-(a7)
+    move.l a4,a0
+
     addq.l #1,d4
-    move.l a4,-(a7)
     lea $ffff8a28.w,a4
     move.w #-1,(a4)+    ; 8a28.w endmask1
     move.w #-1,(a4)+    ; 8a2a.w endmask2
     move.w #-1,(a4)+    ; 8a2c.w endmask3
-    move.w #2,(a4)+     ; 8a2e.w destxinc
-    move.w #34,(a4)+    ; 8a30.w destyinc
-    move.l (a7)+,(a4)+  ; 8a32.l destaddress
-    move.w #64,(a4)+    ; 8a36.w xcount
-    move.w d4,(a4)+     ; 8a38.w ycount
-    move.w #0,(a4)+     ; 8a3a.w hop/op
+    move.w #8,(a4)+     ; 8a2e.w destxinc
+    move.w #40,(a4)+    ; 8a30.w destyinc
+    addq.l #4,a4
+    move.w #16,(a4)+    ; 8a36.w xcount
+    addq.l #2,a4
+    move.w #$f,(a4)+    ; 8a3a.w hop/op
+
+    rept 3
+    move.l a0,-10(a4)   ; 8a32.l destaddress
+    move.w d4,-4(a4)    ; 8a38.w ycount
     move.b #$c0,(a4)    ; 8a3c.b control
+    addq.l #2,a0        ; move to next plane
+    endr
+    move.l a0,-10(a4)   ; 8a32.l destaddress
+    move.w d4,-4(a4)    ; 8a38.w ycount
+    clr.w -2(a4)        ; 8a3a.w hop/op
+    move.b #$c0,(a4)    ; 8a3c.b control
+
+    move.l (a7)+,a0     ; restore a0
+
     jmp $4f290
 

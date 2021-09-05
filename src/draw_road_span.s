e1463949cf6b7edab120b34b7e9d4e836395d266
Jonathan Thomas <jonathan@opalise.co.uk>
2021-09-05 15:05:25 +0100

    backup address reg using usp

--- src/draw_road_span.s
+++ src/draw_road_span.s
@@ -11,18 +11,17 @@ road_span_from_50018:
     addq.l #1,d1              ; we need one more 16 pixel block if coming from 50018
 
 start_span:
-    move.l a0,-(a7)          ; backup a0
     add.w d1,d1              ; number of words for Blitter = number of 16 pixel blocks * 4
     add.w d1,d1
 
+    move.l a0,usp            ; backup a0
     lea $ffff8a32.w,a0
     move.l a4,(a0)+          ; dest address 8a32.l
     addq.l #2,a0
     move.w d1,(a0)+          ; ycount 8a38.w
     addq.l #2,a0
     move.b #$c0,(a0)         ; blitter control 8a3c.b
-
-    move.l (a7)+,a0          ; restore a0
+    move.l usp,a0            ; restore a0
 
     add.w d1,d1              ; bytes to advance = words drawn * 2
     add.l d1,a4              ; advanced dest address to end of span

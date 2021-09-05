141bfb4bbf3971c49f38d36507f127c09bcb8a50
Jonathan Thomas <jonathan@opalise.co.uk>
2021-09-05 12:19:14 +0100

    halftone pattern for road spans

--- src/update_halftone.s
+++ src/update_halftone.s
@@ -1,4 +1,14 @@
 update_halftone:
     move.l #$4e0a0,a5  ; instruction previously located at 4feb6
 
+    move.l a0,-(a7)
+    lea $ffff8a00.w,a0
+
+    rept 4
+    move.l d6,(a0)+
+    move.l d7,(a0)+
+    endr
+
+    move.l (a7)+,a0
+
     rts

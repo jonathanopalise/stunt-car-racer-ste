0e17c5510c4902442419c31566005851b9551402
Jonathan Thomas <jonathan@opalise.co.uk>
2021-09-05 14:57:53 +0100

    optimise/document road span draw

--- src/draw_road_span.s
+++ src/draw_road_span.s
@@ -1,25 +1,19 @@
-; if we land on road_span_loop_start, we draw one more block of 16 pixels than if
-; we land on road_span_loop_end
-; presumably if we land on road_span_loop_end, d1 could be zero
+; inputs:
+; - d1 contains number of 16 pixel spans to be drawn (may be 0)
+; - a4 is the back buffer address to which this span needs to be written
 
 road_span_from_5003c:
     tst.w d1
-    beq.s skip_span
+    beq.s skip_span          ; if number of 16 pixel blocks is 0, do nothing
     bra.s start_span
 
 road_span_from_50018:
-    add.w #1,d1
+    addq.l #1,d1              ; we need one more 16 pixel block if coming from 50018
 
 start_span:
-; if we land on road_span_loop_start, we draw one more block of 16 pixels than if
-; we land on road_span_loop_end
-; do we need to update a4 at the end?
-
-    move.l a0,-(a7)
+    move.l a0,-(a7)          ; backup a0
+    add.w d1,d1              ; number of words for Blitter = number of 16 pixel blocks * 4
     add.w d1,d1
-    add.w d1,d1
-
-    ; destxinc (8a2e), destyinc (8a30), xcount (8a36), hop/op (8a3a) can all be moved
 
     lea $ffff8a32.w,a0
     move.l a4,(a0)+          ; dest address 8a32.l
@@ -28,10 +22,10 @@ start_span:
     addq.l #2,a0
     move.b #$c0,(a0)         ; blitter control 8a3c.b
 
-    move.l (a7)+,a0
+    move.l (a7)+,a0          ; restore a0
 
-    add.w d1,d1
-    add.l d1,a4
+    add.w d1,d1              ; bytes to advance = words drawn * 2
+    add.l d1,a4              ; advanced dest address to end of span
 
 skip_span:
 

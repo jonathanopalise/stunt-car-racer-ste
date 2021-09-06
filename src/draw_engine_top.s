86e93d7d538718ac5a71d8a72566c18fb2230896
Jonathan Thomas <jonathan@opalise.co.uk>
2021-09-06 21:26:04 +0100

    engine OR pass nearly working

--- src/draw_engine_top.s
+++ src/draw_engine_top.s
@@ -1,37 +1,4 @@
 draw_engine_top:  ; start of a line
-    ;move.l a0,a2 
-    ;move.w d1,d3  ; d3 = number of 16 pixel blocks
-
-    ; NEW CODE START
-    ; don't forget to push a4
-
-    ;move.l a4,-(a7)
-    ;lea $ffff8a20.w,a4
-    ;move.w #10,(a4)+         ; srcxinc 8a20.w
-    ;addq.l #2,a4             ; skip srcyinc 8a22.w
-    ;move.l a1,(a4)+          ; source address 8a24.l
-    ;move.w #-1,(a4)+         ; endmask1 8a28.w
-    ;move.w #-1,(a4)+         ; endmask2 8a2a.w
-    ;move.w #-1,(a4)+         ; endmask3 8a2c.w
-    ;move.w #2,(a4)+          ; destxinc 8a2e.w
-    ;addq.l #2,a4             ; skip destyinc 8a30.w
-    ;move.l #$ffff8a00,(a4)+  ; dest address 8a32.l
-    ;move.w #16,(a4)+         ; xcount 8a36.w
-    ;move.w #1,(a4)+          ; ycount 8a38.w
-    ;move.w #$203,(a4)+       ; hop/op 8a3a.w
-    ;move.b #$c0,(a4)         ; blitter control 8a3c.b
-    ;move.l (a7)+,a4
-
-    ; NEW CODE END
-
-    ;lea $a0(a2),a0
-    ;dbra d4,draw_engine_top
-    ;move.w (a7)+,d1
-    ;move.l (a7)+,d5
-    ;rts
-
-    ; START OF REPLICA CODE
-
     move.l d5,-(sp)
     move.w d1,-(sp)
     move.l a4,-(a7)
@@ -105,6 +72,37 @@ label_513e0:
     move.l a0,(a4)           ; dest address 8a32.l
     move.w d2,6(a4)          ; ycount 8a38.w
     move.b d6,10(a4)         ; blitter control 8a3c.b
+
+    ; colour pass
+
+    ;    - srcxinc = 2
+    ;    - srcyinc = 2? (need to skip the mask word after each 4 words written)
+    ;    - sourceaddress = a1 + 2 (to skip initial mask word)
+    ;    - endmask1 = $ffff
+    ;    - endmask2 = $ffff
+    ;    - endmask3 = $ffff
+    ;    - destxinc = 2
+    ;    - destyinc = 0
+    ;    - destinationaddress = a0
+    ;    - xcount = 4
+    ;    - ycount = 16
+    ;    - hop/op = #$0207 (hop = halftone, op = source AND destination, 3 nops)
+    ;    - single pass #$c0 to control
+
+    subq.l #6,a0
+    addq.l #2,a1
+    lea $ffff8a20.w,a4
+    move.w #2,(a4)           ; srcxinc 8a20.w
+    move.w #2,2(a4)          ; srcyint 8a22.w
+    move.l a1,4(a4)          ; source address 8a24.l
+    move.w #2,14(a4)         ; destxinc 8a2e.w
+    clr.w 16(a4)             ; destyinc 8a30.w
+    move.l a0,18(a4)         ; dest address 8a32.l
+    move.w #4,22(a4)         ; xcount 8a36.w
+    move.w #16,24(a4)        ; ycount 8a38.w
+    move.w #$207,26(a4)      ; hop/op 8a3a.w
+    move.b d6,28(a4)         ; blitter control 8a3c.b
+    subq.l #2,a1
 
     ; 256 pixels of 4 bitplane graphics data = 128 bytes
     ; 256 pixels of mask data = 32 bytes
